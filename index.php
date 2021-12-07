14053eb3113af9fbcbb9e853a6bde7408ce110bf
Jagun258 <70840859+Jagun258@users.noreply.github.com>
2021-12-07 10:57:23 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,14 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
     <script src="https://code.jquery.com/jquery-3.6.0.js"
         integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
     <title>HTML CSS JavaScript 07/12/64</title>
 </head>
 
 <body>
-    <h1 id='id'> Halooo </h1>
+    <span id='fname'></span>
+    <span id='lname'></span>
     <button id='btnJSON1'>JSON 1</button>
 </body>
 
@@ -21,18 +21,18 @@
     loadJSON=()=>{
         var url='https://cj-android-demon.herokuapp.com/json1.php'
         $.get(url,(data,status)=>{
-            console.log(data)
+            console.log(jData)
             var jData = JSON.parse(data)
-            console.log(jData.fname)
-            console.log(jData.lname)
+            console.log(jData)
+            $('#fname').text(jData.fname)
+            $('#lname').text(jData.lname)
         })
     }
 
     $(()=>{
-        $('#btnJSON1').click(()=>{
-            loadJSON()
-        })
+        $('#btnJSON1').click(loadJSON)
     })
+
 </script>
 
 </html>
